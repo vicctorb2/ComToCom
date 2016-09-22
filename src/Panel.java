8d91f6db71b712f36fbce8e5dade0fbd8f537be1
vicctorb2 <vicctor.b.0712@gmail.com>
2016-09-22 16:44:08 +0300

    Gui changes

--- src/Panel.java
+++ src/Panel.java
@@ -11,12 +11,12 @@ public class Panel extends JPanel {
     private JTextArea output_field;
     private JTextArea debug_field;
 
-//    static JLabel input_label;
-//    static JLabel output_label;
-//    static JLabel debug_label;
+    static JLabel input_label;
+    static JLabel output_label;
+    static JLabel debug_label;
 
     public Panel(){
-        setLayout(new GridLayout(3,1));
+        setLayout(null);
         setBackground(Color.gray);
 
         input_field = new JTextArea();
@@ -24,35 +24,33 @@ public class Panel extends JPanel {
         debug_field = new JTextArea();
 
         output_field.setEnabled(false);
-        output_field.setBackground(Color.BLUE);
-        output_field.setBackground(Color.cyan);
         debug_field.setEnabled(false);
 
-//        input_label = new JLabel("Input (COM1):");
-//        output_label = new JLabel("Output (COM2):");
-//        debug_label = new JLabel("Debug/Info):");
-//
-//        input_field.setSize(200,50);
-//        output_field.setSize(200,50);
-//        debug_field.setSize(200,100);
-//        input_field.setLocation(50,50);
-//        output_field.setLocation(50,130);
-//        debug_field.setLocation(50,225);
+        input_label = new JLabel("Input (COM1):");
+        output_label = new JLabel("Output (COM2):");
+        debug_label = new JLabel("Debug/Info):");
 
-//        input_label.setSize(100,30);
-//        output_label.setSize(100,30);
-//        debug_label.setSize(100,30);
+        input_field.setSize(200,50);
+        output_field.setSize(200,50);
+        debug_field.setSize(200,100);
+        input_field.setLocation(50,50);
+        output_field.setLocation(50,130);
+        debug_field.setLocation(50,225);
 
-//        input_label.setLocation(50,20);
-//        output_label.setLocation(50,100);
-//        debug_label.setLocation(50,195);
+        input_label.setSize(100,30);
+        output_label.setSize(100,30);
+        debug_label.setSize(100,30);
+
+        input_label.setLocation(50,20);
+        output_label.setLocation(50,100);
+        debug_label.setLocation(50,195);
 
         this.add(input_field);
         this.add(output_field);
         this.add(debug_field);
-//        this.add(input_label);
-//        this.add(output_label);
-//        this.add(debug_label);
+        this.add(input_label);
+        this.add(output_label);
+        this.add(debug_label);
         setListeners();
     }
 
